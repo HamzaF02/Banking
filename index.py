24aeecb2238df575322f46072544956a7f6ce041
Hamza Farah <89578136+HamzaF02@users.noreply.github.com>
2025-09-12 13:56:47 +0200

    Created test cases for classes and debugging

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 class StandardBankAccount:
-    def _init_(self, account_holder, balance):
+    def __init__(self, account_holder, balance):
         self.account_holder = account_holder
         self.balance = balance
     
@@ -9,33 +9,66 @@ class StandardBankAccount:
     def withdraw(self, amount):
         if amount <= self.balance:
             self.balance -= amount
-            return True
         else:
             print("Insufficient funds")
-            return False
     
     def account_info(self):
-        return f"Account Holder: {self.account_holder} \n Balance: {self.balance}"
+        return f"Account Holder: {self.account_holder} \nBalance: {self.balance}"
 
 class SavingsAccount(StandardBankAccount):
     interest_rate = 0.02
-    def _init_(self, account_holder, balance):
-        super(account_holder, balance)
+    def __init__(self, account_holder, balance):
+        super().__init__(account_holder, balance)
     
     def apply_interest(self):
         self.balance =  (1 + self.interest_rate) * self.balance
-    
 
 class CheckingAccount(StandardBankAccount):
     transaction_fee = 1
-    def _init_(self, account_holder, balance):
-        super(account_holder, balance)
+    def __init__(self, account_holder, balance):
+        super().__init__(account_holder, balance)
 
     def withdraw(self, amount):
-        if amount <= self.balance - self.transaction_fee:
-            self.balance -= amount+self.transaction_fee
-            return True
-        else:
-            print("Insufficient funds")
-            return False
+        #Adds transaction fee to the amount to withdraw
+        amount += self.transaction_fee
+        super().withdraw(amount)
+
+
+# Testing of classes
+def main():
+    # Creates a standard bank account
+    standard = StandardBankAccount("Lars",100)
+
+    # Withdraws 30 from the account and prints the account info (Balance should be 70)
+    standard.withdraw(30)
+    print(standard.account_info())
+
+    # Should print "Insufficient funds"
+    standard.withdraw(90)
+
+    # Adds 20 to account and prints the account info (Balance should be 90)
+    standard.deposit(20)
+    print(standard.account_info())
+
+
+    # Creates a savings account
+    saving = SavingsAccount("Jens",100)
+
+    # Applies intrest, then balance should be 102
+    saving.apply_interest()
+    print(saving.account_info()) 
+
     
+    # Creates a checking account
+    checking = CheckingAccount("Abdullah",100)
+
+    # Withdraws 50 from the account and prints the account info (Balance should be 49)
+    checking.withdraw(50)
+    print(checking.account_info()) 
+    checking.withdraw(49) # Should print "Insufficient funds"
+
+
+
+
+if __name__ == "__main__":
+    main()
